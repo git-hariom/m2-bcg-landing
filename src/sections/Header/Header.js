4827ba367a3cb8af52a2b49efb16f636c98f7e82
hariom <>
2022-04-24 00:43:28 +0530

    updated hemberger menu - css based

--- src/sections/Header/Header.js
+++ src/sections/Header/Header.js
@@ -1,48 +1,49 @@
-import './Header.scss';
-import {useState} from 'react';
+import "./Header.scss";
+import { useState } from "react";
 
-const Header = ({prop})=>{
+const Header = ({ prop }) => {
+  const [showHemberger, setShowHemberger] = useState(false);
 
-    const [toggleHemberger, setToggleHemberger] = useState(false);
-    
-    const renderConditionalHeader = () =>{
-        if(!toggleHemberger && window.screen.width<=425){
-            return (
-                <div className="mobile-view-header">
-                    <div className="hemberger" onClick={()=>setToggleHemberger((prev)=>{return !prev})}></div>
-                    <span className='brand-name'>COMMERCE</span>
-                </div>
-            )
-        }else{
-            return (
-                <div className="header-wrapper">
-                    <div className="nav-option-wrapper">
-                        <div className="close" onClick={()=>setToggleHemberger((prev)=>{return !prev})}></div>
-                        <div className="nav-options">
-                            <span className='brand-name'>COMMERCE</span>
-                            <span className='nav'>Sell</span>
-                            <span className='nav'>Marketplace</span>
-                            <span className='nav'>Community</span>
-                            <span className='nav'>Develop</span>
-                            <span className='nav'>Resources</span>
-                        </div>
-                        <div className='btn-actions-wrapper'>
-                            <span className='login'>Log In</span>
-                            <span className='get-started'>Get Started</span>
-                            <span className='search'></span>
-                        </div>
-                    </div>
-            </div>
-            )
-        }
-    }
-    return (
-       <div className="header-parent">
-        {
-            renderConditionalHeader()
-        }
-       </div>   
-    )
-}
+  return (
+    <div className="header-parent">
+      <div className={`mobile-view-header ${!showHemberger ? '':'hide'}`}>
+        <div
+          className="hemberger"
+          onClick={() =>
+            setShowHemberger((prev) => {
+              return !prev;
+            })
+          }
+        ></div>
+        <span className="brand-name">COMMERCE</span>
+      </div>
+      <div className={`header-wrapper ${showHemberger ? '':'hide'}`}>
+        <div className="nav-option-wrapper">
+          <div
+            className="close"
+            onClick={() =>
+              setShowHemberger((prev) => {
+                return !prev;
+              })
+            }
+          ></div>
+          <div className="nav-options">
+            <span className="brand-name">COMMERCE</span>
+            <span className="nav">Sell</span>
+            <span className="nav">Marketplace</span>
+            <span className="nav">Community</span>
+            <span className="nav">Develop</span>
+            <span className="nav">Resources</span>
+          </div>
+          <div className="btn-actions-wrapper">
+            <span className="login">Log In</span>
+            <span className="get-started">Get Started</span>
+            <span className="search"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
